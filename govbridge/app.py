650002ee9ff8cede70c3edb02a82d0b78f472041
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-20 01:01:49 -0400

    Fix GOVBRIDGE startup hook ordering

--- govbridge/app.py
+++ govbridge/app.py
@@ -5,6 +5,7 @@ async def initialize_persistence_on_startup():
     if persistence_configured():
         persistence_init()
 
+
 from fastapi import FastAPI,Header,HTTPException,Request
 from models import BridgeMessage,BridgeResult
 from policy import allowed
